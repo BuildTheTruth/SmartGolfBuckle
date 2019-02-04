80d656fdabfe4fd45ddf995d3f69bd241d7fcb31
Jin <dkwk1254@naver.com>
2019-02-04 19:31:32 +0900

    MarkerClickListener 구현

--- app/src/main/java/com/golflog/smartgolfbuckle/DetailRecordActivity.java
+++ app/src/main/java/com/golflog/smartgolfbuckle/DetailRecordActivity.java
@@ -1,6 +1,7 @@
 package com.golflog.smartgolfbuckle;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.graphics.Bitmap;
@@ -19,22 +20,23 @@ import com.golflog.smartgolfbuckle.vo.ShotData;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Locale;
 
 public class DetailRecordActivity extends AppCompatActivity implements OnMapReadyCallback {
     ActivityDetailRecordBinding binding;
 
     ArrayList<ShotData> mShotDataList;
+    HashMap<String, ShotData> shotDataHashMap;
     GoogleMap mGoogleMap;
 
     View shotView;
@@ -45,6 +47,7 @@ public class DetailRecordActivity extends AppCompatActivity implements OnMapRead
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_detail_record);
+        shotDataHashMap = new HashMap<>();
 
         int hole = getIntent().getIntExtra("SELECTED_HOLE", 0);
         mShotDataList = getIntent().getParcelableArrayListExtra("SELECTED_SHOT_DATA");
@@ -59,17 +62,20 @@ public class DetailRecordActivity extends AppCompatActivity implements OnMapRead
         mGoogleMap = googleMap;
         LatLng mLatLng;
         CameraUpdate mCameraUpdate;
+        String markerId;
 
         setShotMarkerView();
         mGoogleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
 
         for (int idx = 0; idx < mShotDataList.size() - 1; idx++) {
             ShotData shot = mShotDataList.get(idx);
-            addShotMarker(shot, idx);
+            markerId = addShotMarker(shot, idx).getId();
+            shotDataHashMap.put(markerId, shot);
         }
 
         ShotData flag = mShotDataList.get(mShotDataList.size() - 1);
-        addFlagMarker(flag);
+        markerId = addFlagMarker(flag).getId();
+        shotDataHashMap.put(markerId, flag);
         mLatLng = flag.getPosition();
 
         for (int idx = 0; idx < mShotDataList.size() - 1; idx++) {
@@ -80,6 +86,7 @@ public class DetailRecordActivity extends AppCompatActivity implements OnMapRead
 
         mCameraUpdate = CameraUpdateFactory.newLatLngZoom(mLatLng, 16);
         mGoogleMap.animateCamera(mCameraUpdate);
+        mGoogleMap.setOnMarkerClickListener(new MarkerClickListener());
     }
 
     private void setShotMarkerView() {
@@ -93,17 +100,18 @@ public class DetailRecordActivity extends AppCompatActivity implements OnMapRead
         tvCount.setText(String.valueOf(idx + 1));
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(position);
+        markerOptions.snippet(String.valueOf(idx + 1));
         markerOptions.icon(BitmapDescriptorFactory.fromBitmap(createDrawableFromView(this, shotView)));
 
         return mGoogleMap.addMarker(markerOptions);
     }
 
-    private Polyline addPolyline(ShotData shot1, ShotData shot2) {
+    private void addPolyline(ShotData shot1, ShotData shot2) {
         PolylineOptions polylineOptions = new PolylineOptions();
         polylineOptions.add(shot1.getPosition(), shot2.getPosition());
         polylineOptions.width(5);
         polylineOptions.color(Color.RED);
-        return mGoogleMap.addPolyline(polylineOptions);
+        mGoogleMap.addPolyline(polylineOptions);
     }
 
     private Marker addFlagMarker(ShotData flag) {
@@ -130,11 +138,30 @@ public class DetailRecordActivity extends AppCompatActivity implements OnMapRead
         return bitmap;
     }
 
-    private class  MarkerClickListener implements GoogleMap.OnMarkerClickListener {
+    private class MarkerClickListener implements GoogleMap.OnMarkerClickListener {
         @Override
         public boolean onMarkerClick(Marker marker) {
+            String markerId = marker.getId();
+            ShotData mShotData = shotDataHashMap.get(markerId);
+            if (mShotData.getShotGolfClub() == null)
+                return false;
+
+            AlertDialog.Builder dialog = new AlertDialog.Builder(DetailRecordActivity.this);
+            View mView = getLayoutInflater().inflate(R.layout.shot_information, null);
+            dialog.setView(mView);
+
+            TextView tvShotCount = (TextView) mView.findViewById(R.id.tv_shotCount);
+            TextView tvClubKind = (TextView) mView.findViewById(R.id.tv_clubKind);
+            TextView tvDistance = (TextView) mView.findViewById(R.id.tv_distance);
+            TextView tvAltDifference = (TextView) mView.findViewById(R.id.tv_altDifference);
+
+            tvShotCount.setText(String.format(Locale.KOREA, "%d번째 샷정보", Integer.parseInt(marker.getSnippet())));
+            tvClubKind.setText(String.format(Locale.KOREA, "클럽 종류 : %s", mShotData.getShotGolfClub().getKind()));
+            tvDistance.setText(String.format(Locale.KOREA, "샷거리 : %s m", mShotData.getDistance()));
+            tvAltDifference.setText(String.format(Locale.KOREA, "고도차 : %s m", mShotData.getAltDifference()));
+
+            dialog.show();
             return false;
         }
     }
-
 }
